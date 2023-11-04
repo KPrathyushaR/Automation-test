6d855b3bdc9bbd796455485801ac9be03d7c4b50
K Prathyusha <121088489+KPrathyushaR@users.noreply.github.com>
2023-11-04 22:56:17 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -4,23 +4,25 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.select import Select
 driver = webdriver.Chrome()
-driver.get("https://subscription.packtpub.com/advanced-search")
+driver.get("https://subscription.packtpub.com/")
 driver.implicitly_wait(10)
 driver.maximize_window()
 time.sleep(5)
 
-driver.find_element(By.XPATH, "//div[@class='orange advanced-search__search-form__clear-all ml-auto']").click()
+driver.find_element(By.XPATH, "//*[@id='packt-navbar']/div[1]/a").click()
+time.sleep(10)
+#searching the title name in the search bar
+driver.find_element(By.XPATH, "//*[@id='search-input']/input").send_keys("python")
+driver.find_element(By.XPATH, "//*[@id='search-input']/img").click()
 
-#Title of the content
-add = driver.find_element(By.ID, "title").send_keys("python")
-act = ActionChains(driver)
-act.context_click(add).perform()
-print(add)
-
-author = driver.find_element(By.XPATH, "//*[@id='content-author']").send_keys("Aleksander Molak")
-act = ActionChains(driver)
-act.context_click(author).perform()
-print(author)
+#scrolling the starting to the end
+driver.execute_script("window.scrollBy(0,456)", " ")
+value = driver.execute_script("return window.pageYOffset")
+print("Number of pixels moved:", value)
+time.sleep(10)
+# Checkboxes for the product type
+count = driver.find_element(By.CLASS_NAME, "list-item false")
+print(len(count))
 
 
 
